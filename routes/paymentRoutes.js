55cd38e847b50482682f3d8aacf78b8d7be165af
Ashen-Kavinda <ashenarumadura@gmail.com>
2021-09-15 22:27:27 +0530

    payments manage

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,7 +3,7 @@ let payment = require('../models/paymentModel');
 
 router.route("/add").post((req,res)=>{
 
-    console.log("requesttttttttttttttt", req.body);
+    //console.log("requesttttttttttttttt", req.body);
     
     const paymentId = req.body.paymentId;
     const discount =Number(req.body.discount);
@@ -24,7 +24,7 @@ router.route("/add").post((req,res)=>{
         appointmentID
     })
 
-    console.log("newPayment", newpayment);
+    //console.log("newPayment", newpayment);
     newpayment.save().then(()=>{
         res.json("Payment Added")
     }).catch((err)=>{
@@ -35,7 +35,7 @@ router.route("/add").post((req,res)=>{
 })
 
 router.route("/").get((req,res)=>{
-    console.log("get all payment")
+    //console.log("get all payment")
     payment.find().then((payment)=>{
         res.json(payment)
     }).catch((err)=>{
@@ -80,10 +80,10 @@ router.route("/delete/:id").delete(async(req,res)=>{
 })
 
 
-router.route("/get/:id").get(async(req,res)=>{
-    let userId =req.params.id;
-    const user= await Payment.findById(userId).then(()=>{
-        res.status(200).send({status: "fetched", payment:payment})
+router.route("/get/:paymentId").get(async(req,res)=>{
+    let userId =req.params.paymentId;
+    const user= await payment.findOne({paymentId:userId}).then((pay)=>{
+        res.status(200).send({status: "fetched", pay})
     }).catch((err)=>{
         console.log(err.message);
         res.status(500).send({status:"error with getting data", error: err.massage});
