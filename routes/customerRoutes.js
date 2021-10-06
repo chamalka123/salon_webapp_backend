2c3696948d57b02a2ad7cb06bfaa3313c1417ebb
Arunaudayanga <arunaudyanga@gmail.com>
2021-10-06 23:00:20 +0530

    customer login added

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -90,4 +90,67 @@ router.route("/add").post((req,res)=>{
         })
     })
 
+    router.route("/get/:email").get((req, res) => {
+
+        let email = req.params.email;
+        customer.findOne({Email_address:email})
+        .then((customer1) => {
+           res.status(200).send({status: " User fetched", customer1})
+       }).catch(() => {
+            console.log(err.message);
+            res.status(500).send({status:"Error with get user" , error: err.message})
+       })
+   })
+
+    router.route("/signin").post((req,res) => {
+        const custId = req.body.custId;
+        const name = req.body.name;
+        const age = Number(req.body.age);
+        const sex = req.body.sex;
+        const Date_of_birth = req.body.Date_of_birth;
+        const Phone_number = Number(req.body.Phone_number);
+        const Email_address = req.body.Email_address;
+        const password = req.body.password
+        
+            const newCustomer = new customer ({
+                custId,
+                name,
+                age,
+                sex,
+                Date_of_birth,
+                Phone_number,
+                Email_address,
+                password
+        
+            })
+         if(!Email_address || !password){
+             res.status(422).json({error:"Please add email or password"})
+         }
+          customer.findOne({Email_address:Email_address})
+          .then(savedCustomer =>{
+              if(!savedCustomer){
+                 return  res.status(422).json({error:"Invalid Email or Password"})
+        
+              }
+        
+              customer.findOne({password:password})
+              .then(savedCustomer =>{
+                if(savedCustomer){
+                     {/* res.json({message:"successfully signed in"}) */}
+        
+                     res.json(savedCustomer);
+                    
+                    
+                  }
+                  else{
+                      return res.status(422).json({error:"Invalid Email or Password"})
+                  }
+              })
+            .catch(err=>{
+                console.log(err)
+            })
+        
+          })
+        })
+
     module.exports = router;
