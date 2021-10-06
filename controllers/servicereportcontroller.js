b10f94d9b2afa3f2e21ab703ee9bb4fdcd1d0519
chamalka123 <sasiniperera24@gmail.com>
2021-10-06 17:51:50 +0530

    updated service routes and models

--- controllers/servicereportcontroller.js
+++ controllers/servicereportcontroller.js
@@ -35,3 +35,51 @@ exports.viewServicereport= async (req, res) => {
       res.status(500).json({ message: "Error with fetching", error: error.message });
     })
   }
+
+  //update 
+exports.updateServicereport= async (req, res) => { 
+  //fetch id from url
+  let reportid = req.params.id;
+ 
+  const {serviceName, price,date, count,totalPrice,month} = req.body;
+ 
+  const updateServicereport = {
+
+    serviceName,
+    price,
+    date,
+    count,
+    totalPrice,
+    month
+  }
+
+  //check whether there's for the ID
+  try {
+    await Service.findByIdAndUpdate(reportid, updateServicereport);
+
+    //sending the successful status
+    res.status(200).json({ success: true, message: "Updated" })
+  } catch (error) {
+    res.status(500).json({ message: "Error with Updating", error: error.message });
+  }
+}
+//view one
+exports.viewOneServicereport = async (req, res) => {
+  let reportid = req.params.id;
+
+  await Service.findById(reportid).then((serviceReport) => {
+    res.status(200).json({ status: "Service fetched", serviceReport });
+  }).catch((error) => {
+    res.status(500).json({ status: "Error with fetching", error: error.message });
+  })
+}
+//delete existing one
+exports.deleteService = async (req, res) => {
+  let serviceId = req.params.id;
+ 
+  await Service.findByIdAndDelete(serviceId).then(() => {
+    res.status(200).json({ status: "Service Deleted" });
+  }).catch((error) => {
+    res.status(500).json({ status: "Error with Deleting", error: error.message });
+  })
+}
